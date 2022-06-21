7c8ae46f68f6ae9414d25ad1881f58ffab5c18cc
Saisathish <sai.m.r.sathish@gmail.com>
2022-06-22 00:05:16 +0530

    hover button style and centering content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Loaders from './components/Loaders';
+import BorderHoverButton from './components/BorderHoverButton';
 
 function App() {
   return (
     <div className="App">
       <Loaders />
+      <BorderHoverButton />
     </div>
   );
 }
