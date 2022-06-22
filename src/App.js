6e5d774aeb05046057877c7b5d04b8b540389ed2
Saisathish <sai.m.r.sathish@gmail.com>
2022-06-22 23:08:05 +0530

    navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Loaders from './components/Loaders';
 import BorderHoverButton from './components/BorderHoverButton';
+import BottomBarNavigation from './components/BottomBarNavigation';
 
 function App() {
   return (
     <div className="App">
       <Loaders />
       <BorderHoverButton />
+      <BottomBarNavigation />
     </div>
   );
 }
