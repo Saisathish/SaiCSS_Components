c1583f0cbd369dbbaad7c5f04823018cdd7e4ed7
Saisathish <sai.m.r.sathish@gmail.com>
2022-06-24 20:38:58 +0530

    hoverCard

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Loaders from './components/Loaders';
 import BorderHoverButton from './components/BorderHoverButton';
 import BottomBarNavigation from './components/BottomBarNavigation';
 import HamburgerMenu from './components/HamburgerMenu';
+import CardHover from './components/CardHover';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BorderHoverButton />
       <BottomBarNavigation />
       <HamburgerMenu />
+      <CardHover />
     </div>
   );
 }
